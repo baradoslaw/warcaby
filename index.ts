a444d81347f10889f5a97c1a36899ef034162bcd
RadoslawBaran <baradoslaw@gmail.com>
2023-06-17 09:52:05 +0200

    Add connection with DB

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+
 import {handleError} from "./utils/error";
+import './utils/db';
 
 const app = express();
 app.use(cors({
